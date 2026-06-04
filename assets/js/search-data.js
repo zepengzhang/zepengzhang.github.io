ba84b3998e9cfc0fda4b3ff17c277a0ec9d2fdf5
zepengzhang <zhangzp1@shanghaitech.edu.cn>
2026-06-04 12:10:07 +0000

    Deploying to gh-pages from @ zepengzhang/zepengzhang.github.io@0fbf2cad2e26c163010ce5c34b24428ea8dbeca9 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,14 +9,7 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-blog",
-          title: "blog",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/blog/";
-          },
-        },{id: "nav-publications",
+  },{id: "nav-publications",
           title: "Publications",
           description: "Publications in chronological order.",
           section: "Navigation",
